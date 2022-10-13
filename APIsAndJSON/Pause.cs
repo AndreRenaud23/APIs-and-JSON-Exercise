666ed169cf2651527ec1f61ac4d83e92390df474
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-10-13 12:31:02 -0500

    Finised second exercise

---
+++ APIsAndJSON/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    public static class Pause
+    {
+        public static void Slow()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(". ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exercise 1
+
+            //RonVSKanyeAPI.RonQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.KanyeQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.RonQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.KanyeQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.RonQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.KanyeQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.RonQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.KanyeQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.RonQuote();
+
+            //Pause.Slow();
+
+            //RonVSKanyeAPI.KanyeQuote();
+
+            #endregion
+
+            #region Exercise 2
+
+            string key = File.ReadAllText("appsettings.json");
+
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.Write("Please enter your zipcode: ");
+
+            string zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {OpenWeatherMapAPI.GetTemp(apiCall)}° F in your location... have a good day :)");
+
+            #endregion
+
         }
     }
 }
